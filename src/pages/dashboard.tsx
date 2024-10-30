cd5cd23f62ee022391f4c35592567b4fa7a4b1f5
nullgetux <chupunk4@gmail.com>
2024-10-30 17:51:01 +0700

    #3-add-router-validate-islogin

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,10 +1,25 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect } from "react";
+import { useRouter } from 'next/router';
 import { Row } from "react-bootstrap";
 
 //import Components
 import Layout from "@layout/index";
 import BreadcrumbItem from "@common/BreadcrumbItem";
+
 const Dashboard = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+
+    const isAuthenticated = document.cookie.includes('token'); // Sesuaikan dengan nama cookie JWT kamu
+    console.log("cookies:", isAuthenticated)
+
+    if (!isAuthenticated) {
+        // Jika tidak ada token, redirect ke halaman login
+        router.push('/auth/login');
+    }
+}, [router]);
+
     return (
         <>
         <BreadcrumbItem mainTitle="Dashboard" subTitle="Home"  />
